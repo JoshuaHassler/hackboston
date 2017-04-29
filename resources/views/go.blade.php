7eb87efd777b8abbc5ef3b0ef8c5df4f269e4afa
Joshua Hassler <joshua_hassler@student.uml.edu>
2017-04-29 19:43:03 -0400

    hmmm

--- resources/views/go.blade.php
+++ resources/views/go.blade.php
@@ -1,30 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<script>
-window.ButtonWebConfig = {
-  applicationId: 'app-48d2bd5ff23e71f6',
-  enableLogging: true,
-  defaults: {
-    'test': {
-      context: {
-        user_location: {
-          latitude: '40.6827',
-          longitude: '-73.9754'
-        },
-        subject_location: {
-          name: 'Button HQ',
-          latitude: '40.7382869',
-          longitude: '-73.9823721'
-        }
-      }
-    }
-  }
-};
-(function(u,s,e,b,t,n){
-  u['__bttnio']=b;u[b]=u[b]||function(){(u[b].q=u[b].q||[]).push(arguments)};t=s.createElement(e);n=s.getElementsByTagName(e)[0];t.async=1;t.src='https://web.btncdn.com/v1/button.js';n.parentNode.insertBefore(t,n)
-})(window, document, 'script', 'bttnio');
-</script>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -41,4 +17,12 @@ window.ButtonWebConfig = {
         </div>
     </div>
 </div>
+<script>
+window.ButtonWebConfig = {
+  applicationId: 'app-4e830eadf834fbd2'
+};
+(function(u,s,e,b,t,n){
+  u['__bttnio']=b;u[b]=u[b]||function(){(u[b].q=u[b].q||[]).push(arguments)};t=s.createElement(e);n=s.getElementsByTagName(e)[0];t.async=1;t.src='https://web.btncdn.com/v1/button.js';n.parentNode.insertBefore(t,n)
+})(window, document, 'script', 'bttnio');
+</script>
 @endsection
